d0ade87d119c10e60e664773f57d6012de3d2e7b
IgorHenrique07 <140034976+IgorHenrique07@users.noreply.github.com>
2025-10-15 18:58:23 -0300

    Add files via upload

---
+++ BD (1).sql
@@ -0,0 +1,405 @@
+CREATE DATABASE  IF NOT EXISTS `admin_system3` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `admin_system3`;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: admin_system3
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `backup_codes`
+--
+
+DROP TABLE IF EXISTS `backup_codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `backup_codes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `code_hash` varchar(255) NOT NULL,
+  `used` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `backup_codes`
+--
+
+LOCK TABLES `backup_codes` WRITE;
+/*!40000 ALTER TABLE `backup_codes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `backup_codes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Doces','Doces em geral','2025-07-24 00:20:23'),(2,'Lanches','Hambúrguers, sanduíches e similares','2025-07-24 03:34:55'),(3,'Bebidas','Refrigerantes, sucos e bebidas em geral','2025-07-24 03:34:55'),(4,'Sobremesas','Doces, sorvetes e sobremesas','2025-07-24 03:34:55'),(5,'Acompanhamentos','Batatas, saladas e acompanhamentos','2025-07-24 03:34:55');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category_addons`
+--
+
+DROP TABLE IF EXISTS `category_addons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category_addons` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` decimal(10,2) DEFAULT '0.00',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_category_id` (`category_id`),
+  CONSTRAINT `category_addons_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_addons`
+--
+
+LOCK TABLES `category_addons` WRITE;
+/*!40000 ALTER TABLE `category_addons` DISABLE KEYS */;
+INSERT INTO `category_addons` VALUES (9,3,'Gelo',0.00,'2025-09-01 23:39:46','2025-09-01 23:39:46'),(12,2,'Cheddar',2.50,'2025-09-01 23:41:47','2025-09-01 23:41:47');
+/*!40000 ALTER TABLE `category_addons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee_shifts`
+--
+
+DROP TABLE IF EXISTS `employee_shifts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_shifts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `employee_id` int NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `employee_shifts_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee_shifts`
+--
+
+LOCK TABLES `employee_shifts` WRITE;
+/*!40000 ALTER TABLE `employee_shifts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee_shifts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `position` enum('garcom','caixa','cozinha','gerente') NOT NULL,
+  `pin_code` varchar(6) DEFAULT NULL,
+  `status` enum('ativo','inativo','folga') DEFAULT 'ativo',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `cpf` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (3,'asudfhrgidsayfhg','yves@gmail.com','15053852960','1231243124124','cozinha',NULL,'ativo','2025-07-28 23:08:14','2025-07-28 23:08:14');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `goals`
+--
+
+DROP TABLE IF EXISTS `goals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `goals` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `target` decimal(10,2) NOT NULL,
+  `current_amount` decimal(10,2) DEFAULT '0.00',
+  `month` int NOT NULL,
+  `year` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_month_year` (`month`,`year`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `goals`
+--
+
+LOCK TABLES `goals` WRITE;
+/*!40000 ALTER TABLE `goals` DISABLE KEYS */;
+INSERT INTO `goals` VALUES (1,200.00,0.00,7,2025,'2025-07-24 00:19:46'),(2,2000.00,0.00,8,2025,'2025-08-05 06:26:56'),(3,20000.00,0.00,9,2025,'2025-09-24 21:49:42');
+/*!40000 ALTER TABLE `goals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `highlights`
+--
+
+DROP TABLE IF EXISTS `highlights`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `highlights` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `value` decimal(10,2) NOT NULL,
+  `type` enum('sale','goal','product') NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `highlights`
+--
+
+LOCK TABLES `highlights` WRITE;
+/*!40000 ALTER TABLE `highlights` DISABLE KEYS */;
+/*!40000 ALTER TABLE `highlights` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mesa_orders`
+--
+
+DROP TABLE IF EXISTS `mesa_orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mesa_orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mesa_id` int NOT NULL,
+  `order_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `mesa_id` (`mesa_id`),
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `mesa_orders_ibfk_1` FOREIGN KEY (`mesa_id`) REFERENCES `mesas` (`id`),
+  CONSTRAINT `mesa_orders_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mesa_orders`
+--
+
+LOCK TABLES `mesa_orders` WRITE;
+/*!40000 ALTER TABLE `mesa_orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mesa_orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mesas`
+--
+
+DROP TABLE IF EXISTS `mesas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mesas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` int NOT NULL,
+  `capacidade` int NOT NULL DEFAULT '1',
+  `status` enum('disponivel','ocupada','reservada') DEFAULT 'disponivel',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mesas`
+--
+
+LOCK TABLES `mesas` WRITE;
+/*!40000 ALTER TABLE `mesas` DISABLE KEYS */;
+INSERT INTO `mesas` VALUES (19,1,1,'ocupada'),(20,2,3,'ocupada'),(21,3,3,'reservada'),(22,4,6,'disponivel'),(23,5,5,'disponivel');
+/*!40000 ALTER TABLE `mesas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` enum('open','closed') DEFAULT 'open',
+  `total` decimal(10,2) DEFAULT '0.00',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `closed_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'open',500.00,'2025-08-17 07:45:25',NULL),(2,'open',14.00,'2025-08-17 08:08:55',NULL),(3,'open',25.00,'2025-08-17 08:34:36',NULL),(4,'open',25.00,'2025-08-17 08:52:49',NULL),(5,'open',25.00,'2025-08-17 08:54:46',NULL),(6,'open',25.00,'2025-08-17 08:55:25',NULL),(7,'open',5.00,'2025-08-17 08:57:07',NULL),(8,'open',25.00,'2025-08-17 09:09:21',NULL),(9,'open',6.00,'2025-08-17 09:10:17',NULL),(10,'open',24.00,'2025-08-17 10:28:05',NULL),(11,'open',50.00,'2025-08-19 02:03:34',NULL);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` enum('Ativo','Inativo') DEFAULT 'Ativo',
+  `category_id` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Morango do Amo',25.00,'Ativo',1,'2025-07-24 00:20:36'),(2,'Hambúrguer Especial',14.00,'Ativo',2,'2025-07-24 03:34:55'),(3,'Pizza Margherita',18.50,'Ativo',2,'2025-07-24 03:34:55'),(4,'Refrigerante Lata',3.00,'Ativo',3,'2025-07-24 03:34:55'),(5,'Batata Frita Grande',8.00,'Ativo',5,'2025-07-24 03:34:55'),(6,'Sorvete Chocolate',5.00,'Ativo',4,'2025-07-24 03:34:55'),(7,'Suco Natural',4.50,'Ativo',3,'2025-07-24 03:34:55'),(8,'Hambúrguer Simples',12.00,'Ativo',2,'2025-07-24 03:34:55'),(9,'Torta de Limão',6.00,'Ativo',4,'2025-07-24 03:34:55'),(10,'Uva do amor',15.00,'Ativo',1,'2025-08-19 02:06:24'),(11,'X Eggs',14.90,'Ativo',2,'2025-08-19 02:09:15');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sales` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `quantity` int NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `sale_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales`
+--
+
+LOCK TABLES `sales` WRITE;
+/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (1,1,'Morango do Amor',2,250.00,500.00,'2025-08-17 07:45:25'),(2,2,'Hambúrguer Especial',1,14.00,14.00,'2025-08-17 08:08:55'),(3,1,'Morango do Amor',1,25.00,25.00,'2025-08-17 08:34:36'),(4,1,'Morango do Amor',1,25.00,25.00,'2025-08-17 08:52:49'),(5,1,'Morango do Amor',1,25.00,25.00,'2025-08-17 08:54:46'),(6,1,'Morango do Amor',1,25.00,25.00,'2025-08-17 08:55:25'),(7,6,'Sorvete Chocolate',1,5.00,5.00,'2025-08-17 08:57:07'),(8,1,'Morango do Amor',1,25.00,25.00,'2025-08-17 09:09:21'),(9,9,'Torta de Limão',1,6.00,6.00,'2025-08-17 09:10:17'),(10,5,'Batata Frita Grande',3,8.00,24.00,'2025-08-17 10:28:05'),(11,1,'Morango do Amor',2,25.00,50.00,'2025-08-19 02:03:34');
+/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `email` varchar(150) DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_expires` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','$2y$10$VOsjClGjPiD5c/ttQSJvp.LjYgt9dcI.anw5uG1qNTuOaqABjbPx2','2025-07-24 00:19:37','user',NULL,'6cceae3770','2025-10-14 23:35:56');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-14 20:39:16
